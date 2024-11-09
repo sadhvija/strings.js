599805f8590bdc748e844c7b2b14a98a9cdbcdc0
sadhvija <60653845+sadhvija@users.noreply.github.com>
2024-11-09 19:21:12 +0530

    Add files via upload

---
+++ task.js
@@ -0,0 +1,17 @@
+
+let str="hello world";
+console.log(str.replace("world","My Dear Friend"))
+
+let a="Hungry \n"
+console.log(a.repeat(10))
+
+let sentence = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+console.log(sentence.lastIndexOf("fox"))
+
+let message = "Hello, world!"
+console.log(message.startsWith("hi"))
+
+let fileName = "document.pdf";
+console.log(fileName.endsWith(".pdf"))
+
+
